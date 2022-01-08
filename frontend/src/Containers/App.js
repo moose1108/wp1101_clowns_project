36c6e740ceccd022eaa5b82d5b748e491a90f851
unknown <b09902021@ntu.edu.tw>
2022-01-08 21:37:11 +0800

    主畫面

--- frontend/src/Containers/App.js
+++ frontend/src/Containers/App.js
@@ -1,21 +1,23 @@
 import { useState, useEffect, useRef } from 'react'
 import styled from 'styled-components';
+import Home from './Home';
 
 const Wrapper = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-height: 100vh;
-width: 500px;s
-margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 500px;s
+  margin: auto;
 `;
 
 function App() {
   return (
     <Wrapper>
+      <Home />
     </Wrapper>
-  );
+  )
 }
 
 export default App;
---
+++ frontend/src/Containers/Home.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import { Layout, Menu } from 'antd';
+import {
+    BarChartOutlined,
+    SettingOutlined,
+    EnvironmentOutlined,
+    InsertRowAboveOutlined,
+    DollarOutlined,
+    ScheduleOutlined
+} from '@ant-design/icons';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+
+const Home = () => {
+    return (
+        <Layout>
+            <Sider style={{
+                overflow: 'auto',
+                height: '100vh',
+                position: 'fixed',
+                left: 0,
+            }}>
+                <Menu theme="dark" mode="inline">
+                    <Menu.Item style={{ height: '100px', fontSize: '40px', margin: '0px', background: '#FFB326' }}>
+                        +
+                    </Menu.Item>
+                    <Menu.Item style={{ height: '60px', fontSize: '20px', marginTop: '0px' }} icon={<InsertRowAboveOutlined style={{ fontSize: '110%' }} />}>
+                        行事曆
+                    </Menu.Item>
+                    <Menu.Item style={{ height: '60px', fontSize: '20px' }} icon={<BarChartOutlined style={{ fontSize: '110%' }} />}>
+                        圖表
+                    </Menu.Item>
+                    <Menu.Item style={{ height: '60px', fontSize: '20px' }} icon={<DollarOutlined style={{ fontSize: '110%' }} />}>
+                        資產
+                    </Menu.Item>
+                    <Menu.Item style={{ height: '60px', fontSize: '20px' }} icon={<EnvironmentOutlined style={{ fontSize: '110%' }} />}>
+                        地圖
+                    </Menu.Item>
+                    <Menu.Item style={{ height: '60px', fontSize: '20px' }} icon={<ScheduleOutlined style={{ fontSize: '110%' }} />}>
+                        預算
+                    </Menu.Item>
+                    <Menu.Item style={{ height: '60px', fontSize: '20px' }} icon={<SettingOutlined style={{ fontSize: '110%' }} />}>
+                        設定
+                    </Menu.Item>
+                </Menu>
+            </Sider>
+            <Layout style={{ marginLeft: 200 }}>
+                <Header style={{ padding: 0 }} />
+                <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                </Content>
+            </Layout>
+        </Layout >
+    )
+}
+
+export default Home;
