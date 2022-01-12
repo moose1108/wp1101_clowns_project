3ad135e1aa74d5b2612c76ae10aa5334c09cf2d8
moose1108 <75203439+moose1108@users.noreply.github.com>
2022-01-12 14:46:41 +0800

    react-big-calendar to antd

--- frontend/src/Containers/MyCalendar.js
+++ frontend/src/Containers/MyCalendar.js
@@ -1,61 +1,81 @@
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
-import format from "date-fns/format";
-import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
-import getDay from "date-fns/getDay";
+//import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
+import { Button, Input, Calendar, Badge } from "antd";
+import moment from "moment";
+import { tr } from "date-fns/locale";
 
-const locales = {
-    "en-US": require("date-fns/locale/en-US"),
-};
-const localizer = dateFnsLocalizer({
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales,
-});
+// const localizer = momentLocalizer(moment);
 
-const events = [
-    {
-        title: "Big Meeting",
-        allDay: true,
-        start: new Date(2021, 6, 0),
-        end: new Date(2021, 6, 0),
-    },
-    {
-        title: "Vacation",
-        start: new Date(2021, 6, 7),
-        end: new Date(2021, 6, 10),
-    },
-    {
-        title: "Conference",
-        start: new Date(2021, 6, 20),
-        end: new Date(2021, 6, 23),
-    },
-];
 
 const MyCalendar = () => {
-    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
-    const [allEvents, setAllEvents] = useState(events);
-
-    function handleAddEvent() {
-        setAllEvents([...allEvents, newEvent]);
+    const dateCellRender = (value) => {
+        function getListData(value) {
+            let listData;
+            switch (value.date()) {
+              case 8:
+                listData = [
+                  { type: 'warning', content: 'This is warning event.' },
+                  { type: 'success', content: 'This is usual event.' },
+                ];
+                break;
+              case 10:
+                listData = [
+                  { type: 'warning', content: 'This is warning event.' },
+                  { type: 'success', content: 'This is usual event.' },
+                  { type: 'error', content: 'This is error event.' },
+                ];
+                break;
+              case 15:
+                listData = [
+                  { type: 'warning', content: 'This is warning event' },
+                  { type: 'success', content: 'This is very long usual event。。....' },
+                  { type: 'error', content: 'This is error event 1.' },
+                  { type: 'error', content: 'This is error event 2.' },
+                  { type: 'error', content: 'This is error event 3.' },
+                  { type: 'error', content: 'This is error event 4.' },
+                ];
+                break;
+              default:
+            }
+            return listData || [];
+        }
+        const listData = getListData(value);
+        return (
+          <ul className="events">
+            {listData.map(item => (
+              <li key={item.content}>
+                <Badge status={item.type} text={item.content} />
+              </li>
+            ))}
+          </ul>
+        );
     }
-
+    /*function getMonthData(value) {
+        if (value.month() === 8) {
+          return 1394;
+        }
+    }
+    const monthCellRender = (value) => {
+        const num = getMonthData(value);
+        return num ? (
+          <div className="notes-month">
+            <section>{num}</section>
+            <span>Backlog number</span>
+          </div>
+        ) : null;
+    }*/
     return (
-        <Calendar
-            localizer={localizer}
-            events={allEvents}
-            startAccessor="start"
-            endAccessor="end"
-            style={{
-                width: 800,
-                height: 450,
-                margin: "50px"
-            }} />
+        <>
+            <Calendar 
+                onChange={(value) => {
+                    alert(`Your selected ${value.format('YYYY-MM-DD')}`)
+                }}
+                dateCellRender={dateCellRender}
+            />
+        </>
+        
     );
 }
 export default MyCalendar;
