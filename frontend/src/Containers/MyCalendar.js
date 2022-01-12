786ceadad48a0b0177fec9fa096554df7d5d8a6e
moose1108 <75203439+moose1108@users.noreply.github.com>
2022-01-12 20:56:33 +0800

    calendar seleect date

--- frontend/src/Containers/MyCalendar.js
+++ frontend/src/Containers/MyCalendar.js
@@ -1,79 +1,84 @@
-//import { Calendar, momentLocalizer } from "react-big-calendar";
-import "react-big-calendar/lib/css/react-big-calendar.css";
 import React, { useState } from "react";
-import DatePicker from "react-datepicker";
-import { Button, Input, Calendar, Badge } from "antd";
+import { Button, Modal, Calendar, Badge, List, DatePicker } from "antd";
 import moment from "moment";
 import { tr } from "date-fns/locale";
 
-// const localizer = momentLocalizer(moment);
+
+const listData = [
+    { date: "2022-01-07", data: [
+        { status:'success', type: '飲食', content: '國瀚咖哩', cost: 150 },
+        { status:'success', type: '交通', content: '腳踏車拖吊', cost: 50 },
+      ]
+    },
+    { date: "2022-01-09", data: [
+      { status:'success', type: '教育', content: 'SP課本', cost: 1300 },
+      { status:'success', type: '日常用品', content: '學生證不見', cost: 100 },
+      ]
+    }
+];
+
 
 
 const MyCalendar = () => {
+
+    const [ModalVisible, setModalVisible] = useState(false);
+    const [SelectDate, setSelectDate] = useState("");
+    const [AllData, setAllData] = useState(listData);
+
+    function calculate(){
+      let cost = 0;
+      for (let i = 0; i < listData.length; i++)
+        cost += listData[i].cost;
+      return cost;
+    }
+
+    function onPanelChange(value, mode) {
+        const DATE = value.format('YYYY-MM-DD');
+        setSelectDate(DATE);
+        console.log(DATE, typeof(DATE));
+    }
+    
+    const showModal = () => {
+      setModalVisible(true);
+    };
+
+    const handleOk = () => {
+      setModalVisible(false);
+    };
+
+    const handleCancel = () => {
+      setModalVisible(false);
+    };
+
     const dateCellRender = (value) => {
-        function getListData(value) {
-            let listData;
-            switch (value.date()) {
-              case 8:
-                listData = [
-                  { type: 'warning', content: 'This is warning event.' },
-                  { type: 'success', content: 'This is usual event.' },
-                ];
-                break;
-              case 10:
-                listData = [
-                  { type: 'warning', content: 'This is warning event.' },
-                  { type: 'success', content: 'This is usual event.' },
-                  { type: 'error', content: 'This is error event.' },
-                ];
-                break;
-              case 15:
-                listData = [
-                  { type: 'warning', content: 'This is warning event' },
-                  { type: 'success', content: 'This is very long usual event。。....' },
-                  { type: 'error', content: 'This is error event 1.' },
-                  { type: 'error', content: 'This is error event 2.' },
-                  { type: 'error', content: 'This is error event 3.' },
-                  { type: 'error', content: 'This is error event 4.' },
-                ];
-                break;
-              default:
-            }
-            return listData || [];
-        }
-        const listData = getListData(value);
-        return (
-          <ul className="events">
-            {listData.map(item => (
-              <li key={item.content}>
-                <Badge status={item.type} text={item.content} />
-              </li>
-            ))}
-          </ul>
-        );
+      return(
+        <>
+            <Badge status="success" text={calculate()} />
+        </>
+      )
     }
-    /*function getMonthData(value) {
-        if (value.month() === 8) {
-          return 1394;
-        }
-    }
-    const monthCellRender = (value) => {
-        const num = getMonthData(value);
-        return num ? (
-          <div className="notes-month">
-            <section>{num}</section>
-            <span>Backlog number</span>
-          </div>
-        ) : null;
-    }*/
     return (
         <>
-            <Calendar 
-                onChange={(value) => {
-                    alert(`Your selected ${value.format('YYYY-MM-DD')}`)
-                }}
+            <Calendar
+                defaultDate={moment().toDate()}
+                onChange={onPanelChange}
                 dateCellRender={dateCellRender}
+                onSelect={showModal}
             />
+            <Modal title={SelectDate} visible={ModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                
+                {(listData.filter((x) => 
+                  {return x.date === SelectDate}).length === 0 ?
+                   '' : listData.filter((x) =>
+                    {return x.date === SelectDate})[0].data.map(item => (
+                  <li>
+                    <Badge status={item.status} text={item.content + " : " + item.cost + "元"} />
+                  </li>
+                )))
+                  
+                }
+                
+            </Modal>
         </>
         
     );
