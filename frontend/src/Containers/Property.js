85844d99dacb9c0a223ef0da6aa1d9ce83edd312
unknown <b09902021@ntu.edu.tw>
2022-01-15 02:43:04 +0800

    Property.js v1

--- frontend/src/Containers/Property.js
+++ frontend/src/Containers/Property.js
@@ -10,7 +10,8 @@ const Property = ({ username }) => {
 
     const [TotalIncome, setTotalIncome] = useState(0);
     const [TotalOutcome, setTotalOutcome] = useState(0);
-    const [Test, setTest] = useState([]);
+    const [TotalMonth, setTotalMonth] = useState(0);
+    const [Record, setRecord] = useState([]);
     const Date = moment();
     const date = Date.format('YYYY-MM');
 
@@ -20,35 +21,66 @@ const Property = ({ username }) => {
                 username, // give backend
             },
         });
-        setTest(records);
+        setRecord(records);
+    }
+    const handleCalculate = () => {
+        let tempIncome = 0;
+        let tempOutcome = 0;
+        let tempMonth = 0;
+        for (let i = 0; i < Record.length; i++) {
+            let Status = Record[i].status;
+            let cost = Record[i].cost;
+            if (Status === "支出") {
+                tempOutcome += cost;
+                if (Record[i].date_YM === date)
+                    tempMonth -= cost;
+            }
+            else if (Status === "收入") {
+                tempIncome += cost;
+                if (Record[i].date_YM === date)
+                    tempMonth += cost;
+            }
+        }
+        console.log(tempMonth, tempOutcome, tempIncome);
+        setTotalIncome(tempIncome);
+        setTotalOutcome(tempOutcome);
+        setTotalMonth(tempMonth);
+    }
+    const handleColor = (cost) => {
+        if (cost < 0)
+            return "danger";
+        else if (cost > 0)
+            return "success";
     }
     useEffect(() => {
         getData();
     }, [])
-    console.log(Test);
+    console.log(Record);
 
-    
+    useEffect(() => {
+        handleCalculate();
+    }, [Record])
     return (
         <>
             <div style={{ marginTop: '15%' }}></div>
-            <Title>$</Title>
-            <Title >淨資產</Title>
+            <Title type={handleColor(TotalIncome - TotalOutcome)}>${TotalIncome - TotalOutcome}</Title>
+            <Title style={{ marginTop: "1%", marginBottom: "5%" }}>淨資產</Title>
             <Space size={[100, 20]} wrap align='center'>
                 <div>
                     <div>
-                        <Title level={2}>$</Title>
+                        <Title level={2} type='danger'>-${TotalOutcome}</Title>
                     </div>
                     <Title level={2}>總支出</Title>
                 </div>
                 <div>
                     <div>
-                        <Title level={2}>$</Title>
+                        <Title level={2} type='success'>${TotalIncome}</Title>
                     </div>
                     <Title level={2}>總收入</Title>
                 </div>
                 <div>
                     <div>
-                        <Title level={2}>$</Title>
+                        <Title level={2} type={handleColor(TotalMonth)}>${TotalMonth}</Title>
                     </div>
                     <Title level={2}>月收支</Title>
                 </div>
