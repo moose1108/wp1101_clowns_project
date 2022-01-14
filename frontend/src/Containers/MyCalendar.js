9e092dd36afaf6158c5bf17719e3585594fd2a33
unknown <b09902021@ntu.edu.tw>
2022-01-15 03:13:00 +0800

    Calender Css

--- frontend/src/Containers/MyCalendar.js
+++ frontend/src/Containers/MyCalendar.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Button, Modal, Calendar, Badge } from "antd";
+import { Button, Modal, Calendar, Badge, Typography, Tabs } from "antd";
 import axios from '../axios.js'
 import moment from "moment";
 import "../Css/MyCalendar.css";
 
+const { Title, Text } = Typography;
+const { TabPane } = Tabs;
 
 const MyCalendar = ({ username }) => {
   const [ModalVisible, setModalVisible] = useState(false);
@@ -12,16 +14,16 @@ const MyCalendar = ({ username }) => {
   const [GridMode, setGridMode] = useState("month");
   const Get_data = async () => {
     const { data: { records } } = await axios.get('/api/GetUserInformation', {
-        params: {
-          username,
+      params: {
+        username,
       },
     });
     setCurrentRecord(records);
     console.log(records);
   }
-  useEffect(()=>{
+  useEffect(() => {
     Get_data();
-  },[]);
+  }, []);
   function onPanelChange(value, mode) {
     console.log(mode);
     if (mode === "year")
@@ -29,9 +31,9 @@ const MyCalendar = ({ username }) => {
     else
       setGridMode("month");
   }
- 
+
   const showModal = (value) => {
-    if (GridMode === "month"){
+    if (GridMode === "month") {
       const DATE = value.format('YYYY-MM-DD');
       setSelectDate(DATE);
       console.log(SelectDate);
@@ -51,72 +53,68 @@ const MyCalendar = ({ username }) => {
     const eachDate = value.format('YYYY-MM-DD');
     return (
       <>
-        <>
-          {curRecord.filter((x) => 
-            { return x.date === eachDate }).length === 0 ? '' : "支出: "}
-        </>
-        <>
-          {curRecord.filter((x) => 
-          { return x.date === eachDate }).length === 0 ? 
-          '' : curRecord.filter((x) => 
-          { return (x.date === eachDate && x.status === "支出") }).reduce((sum, item) => 
-            sum + item.cost, 0)
-          }
-        </>
-        <br></br>
-        <>
-          {curRecord.filter((x) => 
-            { return x.date === eachDate }).length === 0 ? '' : "收入: "}
-        </>
-        <>
-          {curRecord.filter((x) => 
-            { return x.date === eachDate }).length === 0 ? 
-            '' : curRecord.filter((x) => 
-            { return (x.date === eachDate && x.status === "收入") }).reduce((sum, item) => 
+        <Text type="danger" strong={true}>
+          {curRecord.filter((x) => { return x.date === eachDate }).length === 0 ? '' : "支出: "}
+        </Text>
+        <Text type="danger" strong={true}>
+          {curRecord.filter((x) => { return x.date === eachDate }).length === 0 ?
+            '' : curRecord.filter((x) => { return (x.date === eachDate && x.status === "支出") }).reduce((sum, item) =>
               sum + item.cost, 0)
           }
-        </>
+        </Text>
+        <br></br>
+        <Text type="success" strong={true}>
+          {curRecord.filter((x) => { return x.date === eachDate }).length === 0 ? '' : "收入: "}
+        </Text>
+        <Text type="success" strong={true}>
+          {curRecord.filter((x) => { return x.date === eachDate }).length === 0 ?
+            '' : curRecord.filter((x) => { return (x.date === eachDate && x.status === "收入") }).reduce((sum, item) =>
+              sum + item.cost, 0)
+          }
+        </Text>
       </>
     )
   };
-  
+
   const Model = () => {
-    return(
+    return (
       <>
-        <Modal 
-          title={SelectDate} 
-          visible={ModalVisible} 
-          onOk={handleOk} 
+        <Modal
+          title={SelectDate}
+          visible={ModalVisible}
+          onOk={handleOk}
           onCancel={handleCancel}
         >
-          {curRecord.filter((x) => 
-            { return x.date === SelectDate && x.status === "支出" }).length === 0 ? '' : "支出: "
-          }
-          {curRecord.filter((x) => 
-          { return x.date === SelectDate && x.status === "支出" }).length === 0 ? 
-          '' : 
-          curRecord.filter((x) => { return x.date === SelectDate && x.status === "支出" }).map(item => (
-            <li>
-                <Badge status={"warning"} text={item.type + " : " + item.content + " : " + item.cost + "元"} />
-            </li>
-          ))
-          }
-          {curRecord.filter((x) => 
-            { return x.date === SelectDate && x.status === "收入" }).length === 0 ? '' : "收入: "
-          }
-          {curRecord.filter((x) => 
-          { return x.date === SelectDate && x.status === "收入" }).length === 0 ? 
-          '' : 
-          curRecord.filter((x) => { return x.date === SelectDate && x.status === "收入" }).map(item => (
-            <li>
-                <Badge status={"success"} text={item.type + " : " + item.content + " : " + item.cost + "元"} />
-            </li>
-          ))
-          }
+          <Tabs defaultActiveKey="支出" centered >
+            <TabPane tab="支出" key="支出">
+              {/* {curRecord.filter((x) => { return x.date === SelectDate && x.status === "支出" }).length === 0 ? '' : "支出: "
+              } */}
+              {curRecord.filter((x) => { return x.date === SelectDate && x.status === "支出" }).length === 0 ?
+                '' :
+                curRecord.filter((x) => { return x.date === SelectDate && x.status === "支出" }).map(item => (
+                  <li>
+                    <Badge status={"warning"} text={item.type + " : " + item.content + " : " + item.cost + "元"} />
+                  </li>
+                ))
+              }
+            </TabPane>
+            <TabPane tab="收入" key="收入">
+              {/* {curRecord.filter((x) => { return x.date === SelectDate && x.status === "收入" }).length === 0 ? '' : "收入: "
+              } */}
+              {curRecord.filter((x) => { return x.date === SelectDate && x.status === "收入" }).length === 0 ?
+                '' :
+                curRecord.filter((x) => { return x.date === SelectDate && x.status === "收入" }).map(item => (
+                  <li>
+                    <Badge status={"success"} text={item.type + " : " + item.content + " : " + item.cost + "元"} />
+                  </li>
+                ))
+              }
+            </TabPane>
+          </Tabs>
         </Modal>
       </>
     )
-  } 
+  }
 
   return (
     <>
