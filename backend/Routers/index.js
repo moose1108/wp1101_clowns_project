bdab04e58fd649342ffccb21e4b2a28f9507436f
moose1108 <75203439+moose1108@users.noreply.github.com>
2022-01-15 15:24:34 +0800

    table modal

--- backend/Routers/index.js
+++ backend/Routers/index.js
@@ -4,10 +4,12 @@ import QueryUser from "./Check.js"
 import AddRecord from './AddRecord.js'
 import QueryUserRecords from "./GetUserInformation.js"
 import QueryPie from "./GetPieInformation.js"
+import QueryBudget from "./GetBudgetInformation.js"
 const Router = express.Router()
 Router.post('/Register', (req, res) => { CreateUser(req, res) })
 Router.get('/Check', (req, res) => { QueryUser(req, res) })
 Router.post('/AddRecord', (req, res) => { AddRecord(req, res) })
 Router.get('/GetUserInformation',(req, res)=>{QueryUserRecords(req,res)})
 Router.get('/GetPieInformation',(req, res)=>{QueryPie(req,res)})
+Router.get('/GetBudgetInformation',(req, res)=>{QueryBudget(req,res)})
 export default Router;
