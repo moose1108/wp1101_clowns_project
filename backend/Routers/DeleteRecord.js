55c57053367ddb5c69255d2817e6717fc5fde762
f776655321 <f77665532166@gmail.com>
2022-01-15 15:23:29 +0800

    hi

---
+++ backend/Routers/DeleteRecord.js
@@ -0,0 +1,25 @@
+import { User, Record} from "../Models/Record.js";
+const Delete = async (req,res)=>{
+    let count = 0;
+    const {username, status , content,date} = req.body;
+    // console.log(req.body);
+    let deleted;
+    const user = await User.findOne({username}).populate('records');
+    user.records = await user.records.filter(item=> {
+        if(item.status === status && item.content ===content && count === 0 && item.date ===date)
+        {
+            count +=1;
+            deleted = item;
+            return false
+        }
+        else
+            return true;
+    })
+    console.log("====================================")
+    console.log(user.records);
+    console.log(deleted);
+    await Record.findByIdAndRemove(deleted._id);
+    await user.save();
+    res.send({Message:"Delete Success !"});
+}
+export default Delete
